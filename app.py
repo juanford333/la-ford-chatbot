c77399f254bcf3d95b48e340c3ed72f66c13e02a
juanford333 <laforddewarnes@gmail.com>
2026-03-29 21:39:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from streamlit_gsheets import GSheetsConnection
 # 🔑 CONFIGURACIÓN INICIAL
 # ==========================================
 API_KEY = st.secrets["ANTHROPIC_API_KEY"]
-SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1ClGVOcDcgogynxE8lgNdmJUhzP10UvE-1gpjQKl428k/edit#gid=0"
+
 MI_NUMERO_WHATSAPP = "5491162756333"
 SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1ClGVOcDcgogynxE8lgNdmJUhzP10UvE-1gpjQKl428k/edit"
 client = anthropic.Anthropic(api_key=API_KEY)
